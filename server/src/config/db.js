1384f7075c3b0a832de91e6e975305abc2ae1944
Lidhamulage Fernando <shehanf817@gmail.com>
2026-03-14 22:13:34 +0530

    Implement the db.js file

---
+++ server/src/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    let mongoUri = process.env.MONGO_URI;
+
+    if (!mongoUri) {
+      throw new Error('MONGO_URI is required');
+    }
+
+    const conn = await mongoose.connect(mongoUri);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
